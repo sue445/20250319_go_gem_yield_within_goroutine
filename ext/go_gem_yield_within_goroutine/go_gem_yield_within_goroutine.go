fef74a9eb7a474e12aeeb932fd84e8cbb9203cc0
sue445 <sue445@sue445.net>
2025-03-19 20:24:56 +0900

    [WIP] Add Go

--- ext/go_gem_yield_within_goroutine/go_gem_yield_within_goroutine.go
+++ ext/go_gem_yield_within_goroutine/go_gem_yield_within_goroutine.go
@@ -4,11 +4,13 @@ package main
 #include "go_gem_yield_within_goroutine.h"
 
 VALUE rb_go_gem_yield_within_goroutine_sum(VALUE self, VALUE a, VALUE b);
+VALUE rb_go_gem_yield_within_goroutine_with_block(VALUE self, VALUE arg);
 */
 import "C"
 
 import (
 	"github.com/ruby-go-gem/go-gem-wrapper/ruby"
+	"sync"
 )
 
 //export rb_go_gem_yield_within_goroutine_sum
@@ -21,10 +23,32 @@ func rb_go_gem_yield_within_goroutine_sum(_ C.VALUE, a C.VALUE, b C.VALUE) C.VAL
 	return C.VALUE(ruby.LONG2NUM(sum))
 }
 
+//export rb_go_gem_yield_within_goroutine_with_block
+func rb_go_gem_yield_within_goroutine_with_block(_ C.VALUE, arg C.VALUE) C.VALUE {
+	if ruby.RbBlockGivenP() == 0 {
+		ruby.RbRaise(ruby.VALUE(C.rb_eArgError), "Block not given")
+	}
+
+	var wg sync.WaitGroup
+	wg.Add(1)
+
+	var blockResult ruby.VALUE
+
+	go func() {
+		defer wg.Done()
+		blockResult = ruby.RbYield(ruby.VALUE(arg))
+	}()
+
+	wg.Wait()
+
+	return C.VALUE(blockResult)
+}
+
 //export Init_go_gem_yield_within_goroutine
 func Init_go_gem_yield_within_goroutine() {
 	rb_mGoGemYieldWithinGoroutine := ruby.RbDefineModule("GoGemYieldWithinGoroutine")
 	ruby.RbDefineSingletonMethod(rb_mGoGemYieldWithinGoroutine, "sum", C.rb_go_gem_yield_within_goroutine_sum, 2)
+	ruby.RbDefineSingletonMethod(rb_mGoGemYieldWithinGoroutine, "with_block", C.rb_go_gem_yield_within_goroutine_with_block, 1)
 }
 
 func main() {
